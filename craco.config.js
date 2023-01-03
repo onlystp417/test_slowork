3be881d03522ebf9d1778d2f2511c941260212cc
Jin <onlystp417@gamil.com.tw>
2023-01-03 17:18:28 +0800

    [Fix] Public resouce root path to be dirName/myanmar_the_last_stand

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,15 @@
-const path = require('path')
+const path = require('path');
 module.exports = {
   webpack: {
     alias: {
       '@': path.resolve(__dirname, 'src/'),
       '@COM': '@/components',
       '@HOOK': '@/hook'
+    },
+    configure: {
+      output: {
+        publicPath: '/myanmar_the_last_stand/'
+      }
     }
   }
-}
+};
